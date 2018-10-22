93338d7b2d162f18164e598c5ffe620834643e1d
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-22 21:10:37 +0200

    maint: renamed variable

--- sp2graph/tb.py
+++ sp2graph/tb.py
@@ -17,13 +17,13 @@ __all__ = ['tbBondOrder']
 
 
 def tbBondOrder(A):
-    """ Computes the total bond order between pairs of neighboring atoms.
+    """ Computes the total bond order (BO) between pairs of neighboring atoms.
     Note that the hopping matrix elements in the electronic Hamiltonian are
     negative numbers as they represent the stabilization energy for electrons
     that are allowed to delocalize, i.e., :math:`H_{ij} < 0`.
     """
     w, X = LA.eigh(-1.*A)
-    TB = 1.*A # Begin with the sigma bonds
+    BO = 1.*A # Begin with the sigma bonds
     for i in range(len(w)):
         if w[i] > 0:
             break
@@ -32,8 +32,8 @@ def tbBondOrder(A):
             neig = np.where(A[j, :]==1)[0]
             for idx in neig:
                 # Add contribution from pi-orbital (factor 2 for spin):
-                TB[j, idx] += 2*X[j, i]*X[idx, i]
-    return TB
+                BO[j, idx] += 2*X[j, i]*X[idx, i]
+    return BO
 
 
 def tests():
